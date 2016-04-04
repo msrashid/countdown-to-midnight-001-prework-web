e5c9debdca18ff79ebb1d59fde32234f67e2bf17
msrashid <wlitmilkyway@gmail.com>
2016-04-04 22:08:11 +0000

    fixed countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
 
-def countdown
+def countdown(x)
+
+  while x>=0
+    puts "#{x} SECOND(S)!"
+    x-=1
+    if(x==0)
+      return "HAPPY NEW YEAR!"
+    end
+  end
 end
+
+def countdown_with_sleep(x)
+  while x>=0
+    puts "#{x} SECOND(S)!"
+    sleep 1
+    x-=1
+    if(x==0)
+      return "HAPPY NEW YEAR!"
+    end
+    end
+  end
